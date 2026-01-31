723f12b17ef8c41cc1368eefbf35ae16d154c6cb
Henry Franz <franzhenry46@gmail.com>
2026-01-31 11:16:07 -0300

    feat: mvc do projeto

---
+++ src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
@@ -0,0 +1,24 @@
+package br.com.alurafood.pagamentos.dto;
+
+import br.com.alurafood.pagamentos.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PagamentoDto {
+    private Long id;
+    private BigDecimal valor;
+    private String nome;
+    private String numero;
+    private String expiracao;
+    private String codigo;
+    private Status status;
+    private Long formaDePagamentoId;
+    private Long pedidoId;
+}
